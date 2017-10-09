bbdffd625a65f09d67066b0aa23b00afce1a9718
Patrick <Patrick@DESKTOP-JCHO610.dhcp.wustl.edu>
2017-10-09 12:49:32 -0500

    Studio 6

---
+++ debounce/debounce.ino
@@ -0,0 +1,40 @@
+const int buttonPin = 2;
+int pressed = 0;
+
+int i = 0;
+const unsigned long delta = 1000;
+unsigned long nextTime = 0;
+
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 100;
+
+int buttonState;
+
+void buttonPressed() {
+
+  if((millis()-lastDebounceTime) > debounceDelay) {
+        pressed++;
+        Serial.print(pressed);
+        Serial.print("Interrupt");
+        lastDebounceTime = millis();
+  }
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Interrupts can happen on "edges" of signals.  
+  // Three edge types are supported: CHANGE, RISING, and FALLING 
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, FALLING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int now = millis();
+  
+  if(now>=nextTime){
+    Serial.print(i);
+    i++;
+    nextTime+=delta;
+  }
+}
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -27,10 +27,58 @@ public class SerialComm {
 	}
 		
 	// TODO: Add writeByte() method from Studio 5
+	public void writeByte(byte m) {
+		try {
+			port.writeByte(m);
+		} catch (SerialPortException e) {
+			e.printStackTrace();
+		}
+		if(debug) {
+			//https://stackoverflow.com/questions/1748021/how-to-print-bytes-in-hexadecimal-using-system-out-println
+			System.out.println(String.format("0x%02X", m));
+		}
+	}
 	
 	// TODO: Add available() method
+	public boolean available() {
+		try {
+			if(port.getInputBufferBytesCount() > 0) {
+				return true;
+			}
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 	
 	// TODO: Add readByte() method	
+	public byte readByte() {
+		byte b = 0;
+		
+		try {
+			b = port.readBytes(1)[0];
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(debug) {
+			System.out.println(String.format("0x%02X", b));
+			return b;
+		}else {
+			return b;
+		}
+	}
 	
 	// TODO: Add a main() method
+	public static void main(String[] args) throws SerialPortException {
+		SerialComm comm = new SerialComm("COM4");
+		comm.setDebug(true);
+		while(true) {
+			if(comm.available()) {
+				byte b = comm.readByte();
+				System.out.println((char)b);
+			}
+		}
+	}
 }
